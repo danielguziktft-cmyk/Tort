4654ca25394933256dec9b2034516da67829927f
danielguziktft-cmyk <daniel.guzik.tft@gmail.com>
2026-07-14 23:50:18 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
-export const firebaseConfig={apiKey:"UZUPELNIJ",authDomain:"UZUPELNIJ.firebaseapp.com",databaseURL:"https://UZUPELNIJ-default-rtdb.europe-west1.firebasedatabase.app",projectId:"UZUPELNIJ",storageBucket:"UZUPELNIJ.appspot.com",messagingSenderId:"UZUPELNIJ",appId:"UZUPELNIJ"};
+export const firebaseConfig = {
+  apiKey: "AIzaSyC-uuYK-MvujKYY-2NaSHZjUtRNZiDIhew",
+  authDomain: "licznik-tort.firebaseapp.com",
+  databaseURL: "TUTAJ_BĘDZIE_DATABASE_URL",
+  projectId: "licznik-tort",
+  storageBucket: "licznik-tort.firebasestorage.app",
+  messagingSenderId: "845290922687",
+  appId: "1:845290922687:web:fdcc5e2619a396235adc0a"
+};
