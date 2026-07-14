15f2ec2143c9033afc6783c29f1aa5f6b9ce61c8
danielguziktft-cmyk <daniel.guzik.tft@gmail.com>
2026-07-15 00:01:32 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC-uuYK-MvujKYY-2NaSHZjUtRNZiDIhew",
-  authDomain: "licznik-tort.firebaseapp.com",
-  databaseURL: "TUTAJ_BĘDZIE_DATABASE_URL",
-  projectId: "licznik-tort",
-  storageBucket: "licznik-tort.firebasestorage.app",
-  messagingSenderId: "845290922687",
-  appId: "1:845290922687:web:fdcc5e2619a396235adc0a"
-};
